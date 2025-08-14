eba699e26591497dc9de37ebd6a2e8ad100eeb1d
Xuyhan07 <parlapallysushan@gmail.com>
2025-08-14 17:06:48 +0530

    fix build errors

--- DTO/DashboardDto.cs
+++ DTO/DashboardDto.cs
@@ -4,7 +4,8 @@ namespace TravelDesk.DTO
 {
     public class DashboardDto
     {
+        public int TotalRequests { get; set; }
+        public int PendingRequests { get; set; }
         public List<TravelRequestDto> Requests { get; set; } = new List<TravelRequestDto>();
     }
-
 }
